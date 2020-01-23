be877934c8204eba1fa37f918393334f1c774da5
Sonja Hendricks <sonjaxa@gmail.com>
2020-01-22 17:33:35 -0800

    joe cycle take 1

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var clownList = ["theBros/oneJoe.jpg", "theBros/twoJoe.jpg", "theBros/threeJoe.jpg", "theBros/fourJoe.jpg", "theBros/fiveJoe.jpg", "theBros/sixJoe.jpg"];
+var currentClown = 0;
+
 function youAlreadyKnowWhatsUp() {
   document.getElementById("thingToWatch").innerHTML = "Paragraph changed.";
+}
+
+function clownSwitch() {
+  currentClown = currentClown + 1;
+  if (currentClown == 3) {
+    currentClown = 0;
+  }
+  document.getElementById("firstClown").src = clownList[currentClown];
+}
+
+function setupStart() {
+  setInterval(clownSwitch, 3000);
 }
