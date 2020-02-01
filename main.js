6ad1ba2df7ced82d33980b53ecfeec8511ad5cdf
Sonja Hendricks <sonjaxa@gmail.com>
2020-02-01 11:31:05 -0800

    mutlipke types

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 var clownList = ["theBros/oneJoe.jpg", "theBros/twoJoe.jpg", "theBros/threeJoe.jpg", "theBros/fourJoe.jpg", "theBros/fiveJoe.jpg", "theBros/sixJoe.jpg"];
 var currentClown = 0;
 var beenClicked = false;
+var currentMediaType = "";
 
 // LIST OPTIONS:
 var badTvList =
-["worth it rewatch",
-"CURSED ANSWER: MEATFRUIT",
-"princess movie (i can make this map to a separate list of BPCU movies later)",
-"little women ft willa fitz",
-"reputation reputation lets talk about taylor swifts reputation stadium tour",
-"safiya videos? (i might be able to make something that randomly selects a video out of her uploads. updates to come in later patch updates)",
-"twin peaks",
-"ansel deep divery",
-"the double",
-"whms",
-"dolls?",
-"MAZE RUNNER",
-"divergent",
-"thg"]
+[
+  "worth it rewatch",
+  "CURSED ANSWER: MEATFRUIT",
+  "safiya videos? (i might be able to make something that randomly selects a video out of her uploads. updates to come in later patch updates)",
+  "twin peaks",
+  "dolls?"
+]
+
+var badMoveyList =
+[
+  "princess movie (i can make this map to a separate list of BPCU movies later)",
+  "little women ft willa fitz",
+  "reputation reputation lets talk about taylor swifts reputation stadium tour",
+  "ansel deep divery",
+  "the double",
+  "whms",
+  "maze runner"
+]
 
 function youAlreadyKnowWhatsUp() {
   document.getElementById("thingToWatch").innerHTML = "um he'll choose stuff to watch eventually i guess";
 }
 
 // MEDIA SELECTOR
-function theJellicleChoice() {
-  var garbageVar = Math.floor(Math.random() * badTvList.length);
-  document.getElementById("enterMovieHere").innerHTML = badTvList[garbageVar];
+function theJellicleChoice(list, string) {
+  var garbageVar = Math.floor(Math.random() * list.length);
+  document.getElementById(string).innerHTML = list[garbageVar];
   beenClicked = false;
-  console.log(garbageVar);
+  //console.log(garbageVar);
 }
 
 // THIS DOES THE JOE CYCLE
@@ -40,15 +45,21 @@ function clownSwitch() {
   document.getElementById("theJoes").src = clownList[currentClown];
 }
 
-function buttonClicked() {
+function buttonClicked(mediaType) {
+  currentMediaType = mediaType;
   beenClicked = true;
-  console.log("did it even get checked bro");
+  //console.log("did it even get checked bro");
 }
 
 function checkyCheck() {
   if (beenClicked == true) {
-    theJellicleChoice();
-    console.log("been checky chkeced");
+    if (currentMediaType == "tv") {
+      theJellicleChoice(badTvList, "enterTvHere");
+    }
+    if (currentMediaType == "movey") {
+      theJellicleChoice(badMoveyList, "enterMovieHere");
+    }
+    //console.log("been checky chkeced");
   }
 }
 
